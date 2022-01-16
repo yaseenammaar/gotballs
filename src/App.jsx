88179882f0d146e496646b46ac46678adc7b4fd0
Amir Alam <sharpprogrammer2018@gmail.com>
2022-01-16 03:41:53 -0800

    deesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,49 @@
+
+import { useState } from "react";
+//import { web3 } from '@project-serum/anchor';
+//import DatePicker from "react-date-picker";
+
+import Modal from "./Components/Modal";
+import Button from './Components/Button'
+
+
 import nftImg from "../assets/nft.png";
 import godImg from "../assets/god.png";
 import hero from "../assets/hero.mp4";
 import moonnft from "../assets/moonnft.png";
 import plane from "../assets/plane.png";
 import picasso from "../assets/picasso.png";
-import { useState } from "react";
-//import { web3 } from '@project-serum/anchor';
-
 
 function App() {
   const [isConnected, setIsConnected] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [startDate, setStartDate] = useState(new Date());
 
-  async function connectWallet(){
+  async function connectWallet() {
     try {
       const resp = await window.solana.connect();
-      resp.publicKey.toString()
+      resp.publicKey.toString();
       setIsConnected(true);
-      console.log("Wallet Connected:"+resp.publicKey.toString())
-      } catch (err) {
-        console.log("Error:"+err)
-      }
+      console.log("Wallet Connected:" + resp.publicKey.toString());
+    } catch (err) {
+      console.log("Error:" + err);
+    }
   }
 
-  function disconnectWallet(){
+  function disconnectWallet() {
     setIsConnected(false);
     window.solana.disconnect();
   }
 
- async function mint(){
-    const solConnection = new web3.Connection(getCluster('devnet'));
-    const walletKeyPair = loadWalletKey('./devnet.json');
-    await mintNFT(solConnection, walletKeyPair, "https://gateway.pinata.cloud/ipfs/QmQmvqpZzxPqUHMzuL32S1Pi2eTkF3LYsncWgWK1k52JAQ?preview=1");
+  async function mint() {
+    const solConnection = new web3.Connection(getCluster("devnet"));
+    const walletKeyPair = loadWalletKey("./devnet.json");
+    await mintNFT(
+      solConnection,
+      walletKeyPair,
+      "https://gateway.pinata.cloud/ipfs/QmQmvqpZzxPqUHMzuL32S1Pi2eTkF3LYsncWgWK1k52JAQ?preview=1"
+    );
   }
-
-
 
   return (
     <main>
@@ -46,59 +56,47 @@ function App() {
         <div>
           <a
             href="#"
-            className="md:px-4 px-2 sm:px-2 py-2 text-sm text-gray-500 hover:text-primary "
+            className="px-2 py-2 text-sm text-gray-500 md:px-4 sm:px-2 hover:text-primary "
           >
             About
           </a>
           <a
             href="#"
-            className="md:px-4 px-2 sm:px-2 py-2 text-sm text-gray-500 hover:text-primary "
-          onClick={mint}
+            className="px-2 py-2 text-sm text-gray-500 md:px-4 sm:px-2 hover:text-primary "
+            onClick={mint}
           >
             Contact
           </a>
           <a
             href="#"
-            className="md:px-4 px-2 sm:px-2 py-2 text-sm text-gray-500 hover:text-primary "
+            className="px-2 py-2 text-sm text-gray-500 md:px-4 sm:px-2 hover:text-primary "
             onClick={isConnected ? disconnectWallet : connectWallet}
           >
-            {isConnected ? 'Disconnect' : 'Connect'}
+            {isConnected ? "Disconnect" : "Connect"}
           </a>
         </div>
       </nav>
 
-      {/* Hero */}
-      <section className="px-6 my-16 md:px-28 text-center sm:px-12">
-        <div
-          className="px-8 text-white sm:px-16 py-28 rounded-xl shadow-xl shadow-gray-300"
-          style={{
+      <Modal
+        imgSrc={moonnft}
+        alt="Hello World"
+        isOpen={isModalOpen}
+        setIsOpen={setIsModalOpen}
+      />
 
-            // backgroundImage: `linear-gradient(rgba(92, 187, 239,0.75), rgba(92, 187, 239,0.75)), url("${heroImg}") `,
-          }}
-        >
-          <video
-          autoplay
-          loop
-          muted
-          class="absolute z-10 w-auto min-w-20 min-h-full max-w-none"
-        >
-          <source
-            src={hero}
-            type="video/mp4"
-          />
-          Your browser does not support the video tag.
-        </video>
-          <h1 className="mb-3 text-4xl">Pre-Sale Started!</h1>
-          <p className="mb-6 text-xm">
-            Grab the hold of dates close to your heart.
-          </p>
-          <Button className="hover:shadow-lg">Buy Now</Button>
+      {/* Hero */}
+      <section className="px-6 my-16 text-center md:px-28 sm:px-12">
+        <div className="overflow-hidden shadow-xl rounded-xl shadow-gray-300">
+          <video autoPlay muted={true} loop id="myVideo">
+            <source src={hero} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         </div>
       </section>
 
       {/* About */}
       <section className="px-8 my-24 md:px-28 sm:px-16">
-        <h2 className="mt-8 mb-20 text-lg text-gray-500 font-medium text-center">
+        <h2 className="mt-8 mb-20 text-lg font-medium text-center text-gray-500">
           GoOnDate NFT
         </h2>
 
@@ -109,7 +107,7 @@ function App() {
             </div>
           </div>
 
-          <div className="flex flex-col items-center justify-center text-gray-600 w-full py-16 text-center md:px-12 md:w-11/12 sm:w-10/12 lg:w-3/5">
+          <div className="flex flex-col items-center justify-center w-full py-16 text-center text-gray-600 md:px-12 md:w-11/12 sm:w-10/12 lg:w-3/5">
             How beautiful is the memory of a loved one! But what remains with
             us? A picture? A date? Do you remember the date when you last saw
             them or when you held your baby in your hands for the first time? We
@@ -130,10 +128,7 @@ function App() {
             </a>
           </div>
         </div>
-      
       </section>
-
-      
 
       {/* Skins */}
       <section className="px-8 my-16 md:px-28 sm:px-16">
@@ -143,25 +138,28 @@ function App() {
             type="text"
             name="nftDate"
             id="nftDate"
-            className="rounded px-3 py-2 my-8 border border-gray-300 sm:w-2/3 md:w-1/2 w-10/12 text-gray-600"
+            className="w-10/12 px-3 py-2 my-8 text-gray-600 border border-gray-300 rounded sm:w-2/3 md:w-1/2"
           />
-        </div>
-        
 
-        <h2 className="mt-8 mb-8 text-2xl text-gray-500 font-medium text-center">Dates</h2>
+
+        </div>
+
+        <h2 className="mt-8 mb-8 text-2xl font-medium text-center text-gray-500">
+          Dates
+        </h2>
 
         <div className="flex flex-wrap items-center justify-center">
           {new Array(21).fill(1).map((e) => (
             <div
               key={e}
-              className="cursor-pointer mx-1 my-2 overflow-hidden shadow hover:shadow-lg transition duration-150 shadow-gray-300 rounded-xl"
+              className="mx-1 my-2 overflow-hidden transition duration-150 shadow cursor-pointer hover:shadow-lg shadow-gray-300 rounded-xl"
             >
               <img style={{ width: "150px" }} src={nftImg} alt="" />
             </div>
           ))}
         </div>
 
-        <h2 className="mt-8 mb-8 text-gray-500 mt-16 text-2xl font-medium text-center">
+        <h2 className="mt-8 mt-16 mb-8 text-2xl font-medium text-center text-gray-500">
           Special Dates
         </h2>
 
@@ -169,65 +167,61 @@ function App() {
           {new Array(7).fill(1).map((e) => (
             <div
               key={e}
-              className="cursor-pointer mx-1 my-2 overflow-hidden shadow hover:shadow-lg transition duration-150 shadow-gray-300 rounded-xl"
+              className="mx-1 my-2 overflow-hidden transition duration-150 shadow cursor-pointer hover:shadow-lg shadow-gray-300 rounded-xl"
             >
               <img style={{ width: "150px" }} src={nftImg} alt="" />
             </div>
           ))}
         </div>
-
-       
       </section>
 
       {/* Skins - 2 */}
       <section className="px-8 my-16 md:px-28 sm:px-16">
-        <h2 className="mt-8 mb-8 text-lg text-gray-500 font-medium text-center">
+        <h2 className="mt-8 mb-8 text-lg font-medium text-center text-gray-500">
           Skinned NFTs
         </h2>
 
         <div className="flex flex-wrap items-center justify-center">
-          
-            <div
-              key={1}
-              className=" cursor-pointer mx-1 my-2 overflow-hidden shadow hover:shadow-lg transition duration-150 shadow-gray-400 rounded-xl"
-            >
-              <img style={{ width: "300px" }} src={moonnft} alt="" />
-            </div>
-            <div
-              key={2}
-              className=" cursor-pointer mx-1 my-2 overflow-hidden shadow hover:shadow-lg transition duration-150 shadow-gray-400 rounded-xl"
-            >
-              <img style={{ width: "300px" }} src={plane} alt="" />
-            </div>
-            <div
-              key={2}
-              className=" cursor-pointer mx-1 my-2 overflow-hidden shadow hover:shadow-lg transition duration-150 shadow-gray-400 rounded-xl"
-            >
-              <img style={{ width: "300px" }} src={picasso} alt="" />
-            </div>
-          
+          <div
+            key={1}
+            className="mx-1 my-2 overflow-hidden transition duration-150 shadow cursor-pointer hover:shadow-lg shadow-gray-400 rounded-xl"
+          >
+            <img style={{ width: "300px" }} src={moonnft} alt="" />
+          </div>
+          <div
+            key={2}
+            className="mx-1 my-2 overflow-hidden transition duration-150 shadow cursor-pointer hover:shadow-lg shadow-gray-400 rounded-xl"
+          >
+            <img style={{ width: "300px" }} src={plane} alt="" />
+          </div>
+          <div
+            key={2}
+            className="mx-1 my-2 overflow-hidden transition duration-150 shadow cursor-pointer hover:shadow-lg shadow-gray-400 rounded-xl"
+          >
+            <img style={{ width: "300px" }} src={picasso} alt="" />
+          </div>
         </div>
 
-        <div className="flex flex-wrap text-gray-500 items-center justify-center">
-          <Button className="">Create Skin</Button>
+        <div className="flex flex-wrap items-center justify-center text-gray-500">
+          <Button className="mt-6">Create Skin</Button>
         </div>
       </section>
 
       {/* contact */}
       <section className="px-8 my-16 md:px-28 sm:px-16">
-        <h2 className="mt-8 mb-4 text-lg text-gray-500 font-medium text-center">
+        <h2 className="mt-8 mb-4 text-lg font-medium text-center text-gray-500">
           Contact Us
         </h2>
       </section>
 
       <section className="px-8 my-16 md:px-28 sm:px-16">
-        <h2 className="mt-8 mb-4 text-lg text-gray-500 font-medium text-center">
+        <h2 className="mt-8 mb-4 text-lg font-medium text-center text-gray-500">
           Frequently Asked Questions
         </h2>
       </section>
 
       {/* Footer */}
-      <footer className="flex flex-wrap border-t py-8 md:px-28 items-start justify-center w-full px-8 my-8 sm:px-16 sm:flex-nowrap ">
+      <footer className="flex flex-wrap items-start justify-center w-full px-8 py-8 my-8 border-t md:px-28 sm:px-16 sm:flex-nowrap ">
         <div className="flex flex-col items-center w-full text-sm sm:items-start lg:w-3/6 sm:w-2/4">
           <h3 className="mb-3 text-base font-semibold">Go On Date</h3>
         </div>
@@ -238,7 +232,7 @@ function App() {
             <li className="my-1 text-gray-700">
               <a
                 href="#"
-                className="p-1 hover:text-primary transition duration-150"
+                className="p-1 transition duration-150 hover:text-primary"
               >
                 Instagram
               </a>
@@ -246,7 +240,7 @@ function App() {
             <li className="my-1 text-gray-700">
               <a
                 href="#"
-                className="p-1 hover:text-primary transition duration-150"
+                className="p-1 transition duration-150 hover:text-primary"
               >
                 Twitter
               </a>
@@ -254,7 +248,7 @@ function App() {
             <li className="my-1 text-gray-700">
               <a
                 href="#"
-                className="p-1 hover:text-primary transition duration-150"
+                className="p-1 transition duration-150 hover:text-primary"
               >
                 LinkedIn
               </a>
@@ -262,7 +256,7 @@ function App() {
             <li className="my-1 text-gray-700">
               <a
                 href="#"
-                className="p-1 hover:text-primary transition duration-150"
+                className="p-1 transition duration-150 hover:text-primary"
               >
                 Discord
               </a>
@@ -270,7 +264,7 @@ function App() {
             <li className="my-1 text-gray-700">
               <a
                 href="#"
-                className="p-1 hover:text-primary transition duration-150"
+                className="p-1 transition duration-150 hover:text-primary"
               >
                 Pintrest
               </a>
@@ -278,7 +272,7 @@ function App() {
             <li className="my-1 text-gray-700">
               <a
                 href="#"
-                className="p-1 hover:text-primary transition duration-150"
+                className="p-1 transition duration-150 hover:text-primary"
               >
                 Telegram
               </a>
@@ -286,7 +280,7 @@ function App() {
             <li className="my-1 text-gray-700">
               <a
                 href="#"
-                className="p-1 hover:text-primary transition duration-150"
+                className="p-1 transition duration-150 hover:text-primary"
               >
                 Reddit
               </a>
@@ -300,7 +294,7 @@ function App() {
             <li className="my-1 text-gray-700">
               <a
                 href="#"
-                className="p-1 hover:text-primary transition duration-150"
+                className="p-1 transition duration-150 hover:text-primary"
               >
                 About Us
               </a>
@@ -317,13 +311,6 @@ function App() {
   );
 }
 
-const Button = ({ children, className, ...rest }) => (
-  <button
-    {...rest}
-    className={`px-4 py-2 mt-6 text-sm text-white transition-all duration-300 shadow rounded-md bg-primary hover:shadow-lg shadow-sm`}
-  >
-    {children}
-  </button>
-);
+
 
 export default App;
---
+++ src/Components/Modal.jsx
@@ -0,0 +1,60 @@
+/**
+ *
+ * @param {string} imgSrc - Image URL
+ * @param {string} alt - Image alternate text
+ * @param {boolean} isOpen - state for opening modal
+ * @param {void} setIsOpen - function for managing state for  the modal
+ * @returns
+ */
+
+import Button from "./Button";
+
+export default function Modal({ imgSrc, alt, isOpen, setIsOpen }) {
+  return (
+    <>
+      {isOpen && (
+        <div className="fixed top-0 z-50 flex items-center justify-center w-full min-h-full bg-black bg-opacity-80 backdrop-blur-md ">
+          <div class="lg:w-8/12 md:w-2/5 sm:w-1/2 w-3/5 p-2 relative rounded-xl lg:max-h-auto max-h-[75vh] lg:overflow-y-hidden overflow-y-auto mx-auto flex flex-col lg:flex-row bg-white">
+            <img
+              alt={alt}
+              class="w-full h-full  object-cover lg:h-64 min-h-auto object-center rounded-xl"
+              src={imgSrc}
+            />
+
+            <div class="md:w-auto w-full mt-6 lg:mt-0 md:p-8 p-4">
+              <p>
+                How beautiful is the memory of a loved one! But what remains
+                with us? A picture? A date? Do you remember the date when you
+                last saw them or when you held your baby in your hands for the
+                first time? We feel the same way and it's our desire that you
+                never get loose of the memory that keeps value to you, that is
+                close to your heart. Let's capture those dates together!
+              </p>
+
+              <br />
+              <Button>Buy Me</Button>
+            </div>
+
+            <button
+              onClick={() => setIsOpen(false)}
+              className="absolute p-1 text-xs text-white transition-all duration-300 bg-gray-800 rounded-md shadow-sm top-2 right-2 hover:shadow-lg "
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-4 w-4"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      )}{" "}
+    </>
+  );
+}
