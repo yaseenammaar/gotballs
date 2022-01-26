ab7912954125255c8dfb2809296f7dffc9435fe1
yaseenammaar <yammaar9@gmail.com>
2022-01-26 20:41:49 +0530

    Update

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -4,31 +4,60 @@ export default function Question({ title, children }) {
   const [isActive, setIsActive] = useState(false);
 
   return (
-    <div className="w-full sm:w-4/5 md:w-1/2">
-      <div className="rounded-xl m-0.5 p-5 shadow-gray-300 duration-300 tranition-all hover:shadow-xl">
-        <p className="font-medium text-lg text-gray-900 leading-6">{title}</p>
-        <p className="mt-2">
-          <p className="text-base text-gray-500 leading-6">{children}</p>
-        </p>
+    <div className="w-full sm:w-4/5 md:w-2/3">
+      <div
+        className={`rounded-xl m-1 shadow-gray-300 duration-300 tranition-all hover:shadow-xl ${
+          isActive && "shadow"
+        }`}
+      >
+        <div
+          className="flex items-center justify-between p-5 cursor-pointer "
+          onClick={() => setIsActive(!isActive)}
+        >
+          <p className="text-lg font-medium leading-6 text-gray-900">{title}</p>
+
+          <div className="flex items-center justify-center w-10 ">
+            {isActive ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-5 h-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M5 15l7-7 7 7"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-5 h-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            )}
+          </div>
+        </div>
+        {isActive && (
+          <p className="p-5 mt-2">
+            <span className="text-base leading-6 text-gray-500">
+              {children}
+            </span>
+          </p>
+        )}
       </div>
     </div>
   );
-
-  //   return (
-  //     <div className="w-full sm:w-4/5 md:w-1/2">
-  //       <div className="rounded-xl m-0.5 p-5 shadow-gray-300 duration-300 tranition-all hover:shadow-xl">
-  //         <p
-  //           className="font-medium text-lg text-gray-900 leading-6"
-  //           onClick={() => setIsActive(!isActive)}
-  //         >
-  //           {title}
-  //         </p>
-  //         {isActive && (
-  //           <p className="mt-2">
-  //             <p className="text-base text-gray-500 leading-6">{children}</p>
-  //           </p>
-  //         )}
-  //       </div>
-  //     </div>
-  //   );
 }
