d9dbc9622efb6dcddb36b24adf413cc719ec63eb
Mohd Ahmad <dexk2015@gmail.com>
2022-03-05 15:21:55 +0530

    fixed font not loading in canvas main file - try 1

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,15 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import WarDates from "./pages/WarDates";
 import { Route, Switch } from "wouter";
-
-const WarDates = React.lazy(() => import("./pages/WarDates"));
 
 ReactDOM.render(
   <React.StrictMode>
     <Switch>
       <Route exact path="/" component={App} />
-      <Route exact path="/war-dates">
-        <Suspense
-          fallback={
-            <div class="w-screen h-screen flex items-center justify-center">
-              <h1> Loading... </h1>
-            </div>
-          }
-        >
-          <WarDates />
-        </Suspense>
-      </Route>
+      <Route exact path="/war-dates" component={WarDates} />
 
       <Route>
         <div class="w-screen h-screen flex items-center justify-center">
